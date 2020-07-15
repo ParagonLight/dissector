4c7dc1e045d0c42f5eded18eafb50b1265993106
Jingwei Xu <jingwei.xu.nju@gmail.com>
2020-07-15 05:19:44 +0000

    update by why for public

--- merge_raw_layer_outputs.py
+++ merge_raw_layer_outputs.py
@@ -2,14 +2,15 @@ import os
 import argparse
 import numpy as np
 
-
 import torch
 import utils
+import math
+from sklearn.metrics import roc_auc_score
 
 def compute_sum(ds_root, layers, labels, type, type2, layer_weights, dataset):
     final = [] #torch.zeros((1,3))
     start = 0#30000
-    end = 10000#40000
+    end = 50000#40000
     softmax = None
     out_softmax = None
     sums = None
@@ -20,19 +21,28 @@ def compute_sum(ds_root, layers, labels, type, type2, layer_weights, dataset):
     sum_layer_max = []
     sum_layer_max_value = []
     our_conf = 0.0
+    auc_y = []
+    auc_score = []
     sum = 0
     for index in range(50000):
         result = results[index]
-        print(result)
+        #print(result)
+        if(index%100 == 0):
+            print(index)
         sums = torch.zeros(1, labels)
         out_softmax = torch.load(ds_root+ 'out/' + str(index) + '_' + type2 + '_out_softmax.pt')
         out_softmax = utils.log_softmax_to_softmax(out_softmax.detach()).cpu()
+        if(index<10):
+            print(out_softmax.max().item())
+
         pred_ind = out_softmax.max(1, keepdim=True)[1].item()
         count = 0
         our_conf = 0.0
         flag = 0
         ss = 0
         bonus = 1
+        validity_layer = []
+        our_conf_score = 0.0
         for layer in layers:
             softmax = torch.load(ds_root+ layer + '/' + str(index) + '_' + type2 + '_'+layer+'_' + type + '.pt')
             softmax = utils.log_softmax_to_softmax(softmax.detach()).cpu()
@@ -51,6 +61,17 @@ def compute_sum(ds_root, layers, labels, type, type2, layer_weights, dataset):
             #our_conf += layer_value_pred_ind / layer_value_max
 
             count += 1
+
+            predict_softmax = layer_value_pred_ind
+            max_softmax = layer_value_max # the maximum softmax score for this layer
+            max_label = layer_ind # the corresponding label for the maximum softmax score
+            score = 0.0
+            if pred_ind == max_label:
+                secondHighest = torch.sort(softmax, dim = 0,descending=True)[0][1].item()
+                score = predict_softmax/(predict_softmax + secondHighest + 1e-100)
+            else:
+                score = 1.0 - max_softmax / (max_softmax + predict_softmax + 1e-100)
+            validity_layer.append(score)
         # sorted, index = torch.sort(sums, descending=True)
         # print(result[2], sums)
         # out_softmax = torch.load(ds_root+ folder + str(i) + '_' + type2 + '_out_softmax.pt')
@@ -59,47 +80,116 @@ def compute_sum(ds_root, layers, labels, type, type2, layer_weights, dataset):
         # print(out_softmax.max())
         # print(index)
         # print(result)
-        base = sums[0][int(result[0])]
-        if type == 'dists':
+        our_conf_score = calculate_conf(validity_layer, layer_weights)
+        
+        #base = sums[0][int(result[0])]
+        #if type == 'dists':
 
-            sums = torch.nn.functional.log_softmax(sums)
-            sums = math.e**sums
-            base = sums[0][int(result[0])]
-        sorted, index = torch.sort(sums, descending=True)
-        sum = sums.sum()
-        sum = base / sum
+        #    sums = torch.nn.functional.log_softmax(sums)
+        #    sums = math.e**sums
+        #    base = sums[0][int(result[0])]
+        #sorted, index = torch.sort(sums, descending=True)
+        #sum = sums.sum()
+        #sum = base / sum
+
+        #sum = our_conf_score
 
         flag = 1
         if result[2] == 'incorrect':
             flag = 0
-        v = [index, sum.item(), out_softmax.max().item(), int(result[0]), int(result[1]), flag]
+        auc_y.append(flag)
+        auc_score.append(our_conf_score)
+        v = [index, our_conf_score, out_softmax.max().item(), flag]
 
-        v.extend(sum_layer)
-        v.extend(sum_layer_max)
-        v.extend(sum_layer_max_value)
+        v = [index, our_conf_score, out_softmax.max().item(), int(result[0]), int(result[1]), flag]
+
+        #v.extend(sum_layer)
+        #v.extend(sum_layer_max)
+        #v.extend(sum_layer_max_value)
         final.append(v)
-        del result, sum, base, v
+        del result,  v
         del sum_layer[:]
         del sum_layer_max[:]
         del sum_layer_max_value[:]
     torch.cuda.empty_cache()
     utils.save_tensor(final, ds_root + 'out_' + type  + '_' + type2 + '_{}_{}.pt'.format('_'.join(layers), str(start)))
+    print("AUC score being: ", roc_auc_score(auc_y,auc_score))
+
+
+
+
+def calculate_conf(validity_layer, layer_weights):
+    result = 0.0
+    for i in range(len(validity_layer)):
+        result += validity_layer[i]*layer_weights[i]
+    return result/sum(layer_weights)
+
+def output_weight(weight_x, weightformula, para_alpha, para_beta):
+    if weightformula == 'linear':
+        return output_weight_linear(weight_x,  para_alpha, para_beta)
+    if weightformula == 'log' or weightformula == 'logarithmic':
+        return output_weight_log(weight_x, para_alpha, para_beta)
+    if weightformula == 'exp' or weightformula == 'exponential':
+        return output_weight_exp(weight_x, para_alpha, para_beta)
+
+def output_weight_linear(weight_x,  para_alpha, para_k):
+    layer_weight = weight_x
+    if para_alpha == 0 and para_k == 0: # y = ax+k
+        for i in range(len(weight_x)):
+            layer_weight[i] = weight_x[i]
+    if para_alpha != 0: #y = x
+        for i in range(len(weight_x)):
+            layer_weight[x] = para_alpha * weight_x[i] + 1
+    return layer_weight
+
+def output_weight_log(weight_x, para_alpha, para_beta):
+    layer_weight = weight_x;
+    if para_alpha == 0 and para_beta == 0: # y = lnx
+        for i in range(len(weight_x)):
+            layer_weight[i] = math.log(weight_x[i], math.e)
+    if para_alpha != 0 and para_beta == 0: # y = alnx + 1
+        for i in range(len(weight_x)):
+            layer_weight[i] = math.log(weight_x[i], math.e) * para_alpha + 1
+    if para_alpha != 0 and para_beta != 0: # y = aln(bx+1)+1
+        for i in range(len(weight_x)):
+            layer_weight[i] = math.log(weight_x[i]*para_beta+1)*para_alpha + 1
+    return layer_weight
+
+def output_weight_exp(weight_x, para_alpha, para_beta):
+    layer_weight = weight_x
+    if para_alpha == 0: # y = e ^ (bx)
+        for i in range(len(weight_x)):
+            layer_weight[i] = math.exp(para_beta * weight_x[i]) # y e^(bx)
+    if para_alpha != 0: # y = a e * (bx) + 1
+        for i in range(len(weight_x)):
+            layer_weight[i] = math.exp(para_beta * weight_x[i] ) * para_alpha + 1
+    return layer_weight
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Embedding extraction module')
     parser.add_argument('--dataset', default='imagenet',
-                        help='dataset (default=mnist)')
+                        help='dataset (default=imagenet)')
     parser.add_argument('--type', default='softmax',
                         help='value type (default=softmax)')
     parser.add_argument('--gpu-id', default='1', type=str,
                         help='id(s) for CUDA_VISIBLE_DEVICES')
+    parser.add_argument('--weightformula', default = 'linear',
+                        help='weightformula (default=linear)')
+    parser.add_argument('--para_alpha', default = '0', type = str,
+                        help = 'para_alpha (default = 0)')
+    parser.add_argument('--para_beta', default = '0', type = str,
+                        help = 'para_beta (default = 0)')
     args = parser.parse_args()
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
 
     dataset = args.dataset
     values = 10000
     type = args.type
+    weightformula = args.weightformula
+    para_alpha = args.para_alpha
+    para_beta = args.para_beta
+
     root = '/data/xjw/anatomy/'
     type2 = 'clean'
     ds_root = root + dataset + '/tensor_pub/'
@@ -107,9 +197,11 @@ if __name__ == '__main__':
     labels = 1000
     values = 50000
     layers = ['res_layer1', 'res_layer2', 'res_block8', 'res_block16', 'res_layer3', 'res_layer4', 'out']
-    layer_weights = [0.05,0.1,0.15,0.2,0.2,0.1,1]
+    #layer_weights = [0.05,0.1,0.15,0.2,0.2,0.1,1]
     # folders = ['10000/','20000/', '30000/', '40000/', '50000/']
-    print(layer_weights)
+    #print(layer_weights)
+    weight_x = [1,2,3,4,5,6,7]
+    layer_weights = output_weight(weight_x, weightformula, float(para_alpha), float(para_beta))
     compute_sum(ds_root, layers, labels, type, type2, layer_weights, dataset)
     # show_results(ds_root, values, 'dists', 1)
     # show_results(ds_root, values, type, 1, type2, layers, layer_weights)
